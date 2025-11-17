5f04284f9607629a09e0abc53420414af586e83d
Alexandru Iuga <alex.iuga@gmail.com>
2025-11-17 18:14:58 +0200

    removed all projects

--- sources/AspNetCoreAuthenticationDemo/Models/TokenApiRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DustInTheWind.AspnetCoreAuthenticationDemo.Models;
-
-public class TokenApiRequest
-{
-    public string Username { get; set; }
-
-    public string Password { get; set; }
-}
--- sources/AspNetCoreAuthenticationDemo/Models/TokenApiResponse.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DustInTheWind.AspnetCoreAuthenticationDemo.Models;
-
-internal class TokenApiResponse
-{
-    public string Token { get; set; }
-}
