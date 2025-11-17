50054425cbe3d84967243606e1d486797189d09f
Alexandru Iuga <alex.iuga@gmail.com>
2025-11-17 18:28:23 +0200

    Added a new project

---
+++ sources/AspNetCoreAuthenticationDemo/Controllers/DemoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DustInTheWind.AspNetCoreAuthenticationDemo.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DemoController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Hello, World!");
+    }
+}
