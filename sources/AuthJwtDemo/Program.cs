5b4bf0176737e377e08cc41a06ec934e5e7260db
Alexandru Iuga <alex.iuga@gmail.com>
2026-03-12 14:43:44 +0200

    rearranged authnetication middleware

--- sources/AuthJwtDemo/Program.cs
+++ sources/AuthJwtDemo/Program.cs
@@ -42,9 +42,9 @@ public class Program
             app.MapOpenApi();
         }
 
-        app.UseAuthentication();
         app.UseHttpsRedirection();
 
+        app.UseAuthentication();
         app.UseAuthorization();
 
         app.MapControllers();
